68904a7e3aa40f305d76ce92bdbb593f8fb21f7c
Shashank Shekhar <82131342+Shashank7-S@users.noreply.github.com>
2023-09-15 00:25:36 +0530

    Update moongoose.js

--- config/moongoose.js
+++ config/moongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/API', {
+mongoose.connect('mongodb+srv://shashank:shashank@cluster0.rwjak24.mongodb.net/api?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
